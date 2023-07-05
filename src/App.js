d8e618d90c470d227eb354917dd02fc8ac9e8a04
tnelson2016 <troy.nelson@bracari.com>
2023-07-05 16:07:15 -0400

    finished responsive weather app

--- src/App.js
+++ src/App.js
@@ -6,30 +6,57 @@ import { useEffect, useState } from 'react'
 import { getFormattedWeatherData } from './weatherService'
 
 function App() {
+    const [city, setCity] = useState('Charleston')
     const [weather, setWeather] = useState(null)
     const [units, setUnits] = useState('imperial')
+    const [bg, setBg] = useState(hotBg)
 
     useEffect(() => {
         const fetchWeatherData = async () => {
-            const data = await getFormattedWeatherData('paris', units)
+            const data = await getFormattedWeatherData(city, units)
             setWeather(data)
+
+            //dynamic background
+            const threshold = units === 'metric' ? 20 : 78
+            if (data.temp <= threshold) setBg(coldBg)
+            else setBg(hotBg)
         }
 
         fetchWeatherData()
-    }, [])
+    }, [units, city])
+
+    //Changes button from °F to °C
+    const handleUnitsClick = (e) => {
+        const button = e.currentTarget
+        const currentUnit = button.innerText.slice(1)
+        const isCelsius = currentUnit === 'C'
+        button.innerText = isCelsius ? '°F' : '°C'
+        setUnits(isCelsius ? 'metric' : 'imperial')
+    }
+    const enterKeyPressed = (e) => {
+        if (e.keyCode === 13) {
+            setCity(e.currentTarget.value)
+            e.currentTarget.blur()
+        }
+    }
 
     return (
-        <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+        <div className="app" style={{ backgroundImage: `url(${bg})` }}>
             <div className="overlay">
                 {weather && (
                     <div className="container">
+                        <h1 className="title">Troy's Real Time Weather App</h1>
+
                         <div className="section section__inputs">
                             <input
+                                onKeyDown={enterKeyPressed}
                                 type="text"
                                 name="city"
                                 placeholder="Enter City..."
                             />
-                            <button>°F</button>
+                            <button onClick={(e) => handleUnitsClick(e)}>
+                                °F
+                            </button>
                         </div>
                         <div className="section section__temperature">
                             <div className="icon">
